38bae6cae58e02726f263b07b0e491522488032a
Jeff Wang <a625687551@gmail.com>
2017-01-14 19:46:38 +0800

    设置好了下载问题

--- meizitu/pipelines.py
+++ meizitu/pipelines.py
@@ -14,24 +14,26 @@ from scrapy.pipelines.images import ImagesPipeline
 
 class MeizituPipeline(object):
     def process_item(self, item, spider):
-        pass
+        return item
 
 class DownloadImagesPipeline(ImagesPipeline):
 
     def get_media_requests(self,item,info):#下载图片
         for image_url in item['image_url']:
-            yield Request(image_url,meta={'name':item['image_name']})
+            yield Request(image_url,meta={'item':item})
 
     def item_completed(self,results,item,info):
         file_paths=[x['path'] for ok,x in results if ok]
         # image_name=request
         # file_paths=item['file_paths']
         if not file_paths:
-            raise DropItem(u'图片未下载好{}'.format())
+            raise DropItem(u'图片未下载好{}'.format(file_paths))
         item['file_paths']=file_paths
         return item
-    def file_path(self, request, response=None, info=None):
+    def file_path(self, request, response=None, info=None):#通过这个来对图片进行命名
         item=request.meta['item']
+        image_guid=request.url.split('/')[-1][:-4]
+        return 'full/{0}/{1}.jpg'.format(item['file_paths'],image_guid)
 
 
 
