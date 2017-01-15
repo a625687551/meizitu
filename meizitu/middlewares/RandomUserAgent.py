eded71f772da56a177d1f21afcb4bb79d2d3d860
Jeff Wang <a625687551@gmail.com>
2017-01-15 09:12:04 +0800

    添加UA尝试解决SS连接内地网站被封

---
+++ meizitu/middlewares/RandomUserAgent.py
@@ -0,0 +1,16 @@
+import random
+from meizitu.settings import USER_AGENTS
+
+class RandomUserAgent(object):
+    '''
+    这个类主要随机产生UA
+    '''
+    def __init__(self):
+        self.agents=USER_AGENTS
+
+    # @classmethod
+    # def from_crawler(cls,crawler):
+    #     return cls(crawler.setting.getlist('USER_AGENTS'))
+
+    def process_request(self,request,spider):
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
